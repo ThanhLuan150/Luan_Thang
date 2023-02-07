c4b152424c066effae3f82a6c43139ebd55b9772
ThanhLuan150 <letruongthanhluan1505@gmail.com>
2023-02-07 11:58:21 +0700

    dsdsds

--- for.php
+++ for.php
@@ -5,19 +5,33 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Vòng lặp for</title>
+    <style>
+        .do{
+            color:red;
+            background: red;
+        }
+        .xanh{
+            color:green;
+            background: green;
+        }
+    </style>
 </head>
 <body>
-<?php 
-    for ($i=0; $i < 10; $i++) {
-        for ($i=0; $i < 10 ; $i++) { 
-             if($i!=0){
-                echo "<div class='do'>do</div>";
-             }
-             else{
-                echo "<div class='xanh'> xanh</div>";
-             }
+<?php
+        for ($a = 0; $a <= 5; $a++){
+            for($b = 0; $b <= 5; $b++) {
+                if ($a % 2 == 0) {
+                    echo "<div style='background:blue; margin:10px; width:50px; height:50px;display:inline-block;'></div>";
+                }
+                else {
+                    echo "<div style='background:red; margin:10px; width:50px; height:50px;display:inline-block;'></div>";
+                }
+            }
+            echo '<br>';
         }
-        }
-    ?>
+    ?> <br> <br>
+
+
 </body>
 </html>
+
