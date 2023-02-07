f03e8ef86c82360367ec0ee2eac9aec90c4824bd
ThanhLuan150 <letruongthanhluan1505@gmail.com>
2023-02-07 11:10:42 +0700

    ss

--- for.php
+++ for.php
@@ -7,6 +7,17 @@
     <title>Vòng lặp for</title>
 </head>
 <body>
-    
+<?php 
+    for ($i=0; $i < 10; $i++) {
+        for ($i=0; $i < 10 ; $i++) { 
+             if($i!=0){
+                echo "<div class='do'>do</div>";
+             }
+             else{
+                echo "<div class='xanh'> xanh</div>";
+             }
+        }
+        }
+    ?>
 </body>
 </html>
